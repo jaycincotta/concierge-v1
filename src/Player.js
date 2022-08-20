e8c16817b16889895277062bbfd677e8b7c9bde5
Jay Cincotta <jay.cincotta@gmail.com>
2022-08-20 09:18:31 -0700

    Debugging playback on iPhone

--- src/Player.js
+++ src/Player.js
@@ -7,6 +7,14 @@ const Player = ({src}) => (
       url={src}
       className="react-player"
       playing={true}
+      muted={false}
+      config={{
+        vimeo: {
+          iframeParams: { fullscreen: 0 },
+          autoplay: true,
+          playsinline: true,
+        },
+      }}
       width="98%"
       height="98%"
       controls={true}
