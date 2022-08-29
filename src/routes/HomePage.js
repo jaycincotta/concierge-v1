e28ff8a5a9af559c43a356d102b6d28bf8e45781
Jay Cincotta <jay.cincotta@gmail.com>
2022-08-29 05:23:02 -0700

    Fix compiler warning

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 import Task from "../layout/Task"
 import pick from "../functions/pick"
 
