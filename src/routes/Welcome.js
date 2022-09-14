5a3e16f67596314e954fa1c04baf5d05136330fb
Jay Cincotta <jay.cincotta@gmail.com>
2022-09-13 20:29:47 -0700

    WIP

--- src/routes/Welcome.js
+++ src/routes/Welcome.js
@@ -13,7 +13,7 @@ export default function Welcome() {
                         <h1>Welcome to Case Parts!</h1>
                         <div className="subtitle">The Commercial Refrigeration Parts Experts</div>
                         <p>
-                            We make it easy for you to get <b>commercial refrigeration parts</b> quickly and at a fair price -- even when you're not sure what parts you need!
+                            We make it easy for you get parts <b>quickly and at a fair price</b> -- even when you're not sure what parts you need!
                         </p>
                         <p>
                             We're a family-owned business spanning four generations and 50 years of service with three locations nationwide to get you your parts <b>faster and cheaper</b>.
@@ -22,7 +22,7 @@ export default function Welcome() {
                             Our Customer Service Reps average more than 12 years experience at Case Parts providing <b>exceptional customer service</b>. They know what questions to ask and where to go to solve your problems!
                         </p>
                         <p>
-                            We appreciate you giving us a chance to provide <b>exceptional customer service</b>.
+                            We appreciate you giving us a chance to serve you.
                         </p>
                         <h2>How can we help you today?</h2>
                         <div className="pad center">
