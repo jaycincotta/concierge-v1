8dd3fc7b3367c665b37a6d1b80bba430920ee45e
Jay Cincotta <jay.cincotta@gmail.com>
2022-09-14 08:39:32 -0700

    WIP

--- src/routes/branch/MPK.js
+++ src/routes/branch/MPK.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, Navigate } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 import Task from "../../layout/Task"
 import TaskStep from "../../layout/TaskStep"
 
