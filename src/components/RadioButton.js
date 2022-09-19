3140c4ee5a22a654eb6a27802af315816309852a
Jay Cincotta <jay.cincotta@gmail.com>
2022-09-18 18:12:04 -0700

    Refined RadioButton

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -2,9 +2,14 @@ import React from "react"
 
 export default function RadioButton({ label, name, value, setter }) {
     return (
-        <div className="radioButton" onClick={() => setter(name) }>
+        <div className="radioButton" onClick={() => setter(name)}>
             <input type="radio" name={name} value={value}
-                checked={value === name} />
+                checked={value === name}
+                // We need an onCHange handler to avoid a runtime warning, but
+                // we don't need to do anything at this level because the 
+                // parent div will handle it.
+                onChange={(e) => { return null }}
+            />
             <div>{label}</div>
         </div>
     )
