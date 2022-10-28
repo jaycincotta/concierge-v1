15016ca8fa2ed88f2ba1682456080746790c3a35
Jay Cincotta <jay.cincotta@gmail.com>
2022-10-28 09:09:45 -0700

    Added script to compare shared files

---
+++ compare.ps1
@@ -0,0 +1,3 @@
+# Shared logic for calling API enpoints with proper authentication
+Compare-Content ./src/context/AuthContext.js,../AuthClient/src/context/AuthContext.js
+Compare-Content ./src/context/AuthProvider.js,../AuthClient/src/context/AuthProvider.js
